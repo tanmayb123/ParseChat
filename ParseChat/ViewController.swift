5c5e9497c35fb87947756e317f678f22a10fde77
Tanmay Bakshi <tajymany@gmail.com>
2015-10-10 12:02:46 -0400

    Initial Commit

---
+++ ParseChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseChat
+//
+//  Created by Tanmay Bakshi on 2015-10-10.
+//  Copyright © 2015 Tanmay Bakshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
